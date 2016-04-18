c0ae090568e0a1a8261583085a1d7f008928fd91
dinoeL <leonid.zhdanok@gmail.com>
2016-04-18 22:27:19 +0300

    new
    Новая метка в которой показывается вывод предыдущих операций + обработка значений с клавиш.

--- calculator.cpp
+++ calculator.cpp
@@ -1,9 +1,11 @@
 #include "calculator.h"
 
 Calculator::Calculator(QWidget *parent) : QWidget(parent){
-
+    firststring = new QLabel("");
+    firststring->setMinimumSize(150,50);
     displaystring = new QLabel("");
     displaystring->setMinimumSize(150, 50);
+    displaystring->setFont(QFont("sans", 14, QFont::Bold));
     QChar aButtons[4][4] = {
      {'7', '8', '9', '/'},
      {'4', '5', '6', '*'},
@@ -13,11 +15,12 @@ Calculator::Calculator(QWidget *parent) : QWidget(parent){
 
 
     QGridLayout *myLayout = new QGridLayout;
-    myLayout->addWidget(displaystring, 0, 0, 1, 4);
-    myLayout->addWidget(createButton("C"), 1, 3);
+    myLayout->addWidget(firststring, 0, 0, 1, 4);
+    myLayout->addWidget(displaystring, 1, 0, 1, 4);
+    myLayout->addWidget(createButton("C"), 2, 3);
     for (int i = 0; i < 4; ++i){
         for (int j = 0; j < 4; ++j){
-            myLayout->addWidget(createButton(aButtons[i][j]), i+2, j);
+            myLayout->addWidget(createButton(aButtons[i][j]), i+3, j);
         }
     }
     setLayout(myLayout);
@@ -25,7 +28,7 @@ Calculator::Calculator(QWidget *parent) : QWidget(parent){
 
 QPushButton* Calculator::createButton (const QString str) {
     QPushButton* pcmd = new QPushButton(str);
-    pcmd->setMinimumSize(40, 40);
+    pcmd->setMinimumSize(40, 30);
     connect(pcmd, SIGNAL(clicked()), this, SLOT(slotButtonClicked()));
  return pcmd;
 }
@@ -39,44 +42,59 @@ void Calculator::calculate() {
     else if (strOperation == "-") { dResult = dOperand1 - dOperand2; }
     else if (strOperation == "/") { dResult = dOperand1 / dOperand2; }
     else if (strOperation == "*") { dResult = dOperand1 * dOperand2; }
-    displaystring->setText(QString("%1").arg(dResult, 0, 'f', 6)); //6 znakov after ","
+    displaystring->setText(QString("%1").arg(dResult, 0, 'f', 3)); //3 znakov after ","
 }
 
-void Calculator::slotButtonClicked() { // reagir to key presses and ypr the stack
-    QString str = ((QPushButton*)sender())->text();
-    if (str == "C") {
-        stack.clear();
-        displaystring->setText("");
-        return;
-    }
-
+void Calculator::input(QString str) {
     QString text = displaystring->text();
     int len = text.length();
     QString last = "";
     if (len>0) last = text.right(1);
+     if (((len==0 && stack.count()==0) ||
+        ((stack.count()==2 && len>1 && (last=="+"||last=="-"||last=="*"||last=="/"))))
+        && (str.contains(QRegExp("[0-9]")) || str=="-")) {
 
-    if (((len==0 && stack.count()==0) ||
-          ((stack.count()==2 && len>1 && (last=="+"||last=="-"||last=="*"||last=="/")))) &&
-         (str.contains(QRegExp("[0-9]")) || str=="-")) {
+        firststring->setText(text);
         text=str;
-    }
-    else if ((text+str).contains(QRegExp("^-?[0-9]+\\.?[0-9]*$"))) {
+     }
+
+        else if ((text+str).contains(QRegExp("^-?[0-9]+\\.?[0-9]*$"))) {
         text+=str;
-    }
+ }
     else if (text.contains(QRegExp("^-?[0-9]+\\.?[0-9]*$"))) {
-        if (str=="*"||str=="/"||str=="+"||str=="-"||str=="=") {
-            if (stack.count()==2) {
-                stack.push(text);
-                calculate();
-                text=displaystring->text();
-            }
-            if (str!="=") {
-                stack.push(text);
-                text+=str;
-                stack.push(str);
-            }
-        }
-    }
-    displaystring->setText(text);
+    if (str=="*"||str=="/"||str=="+"||str=="-"||str=="=") {
+        if (stack.count()==2) {
+            stack.push(text);
+            firststring->setText(firststring->text()+text+"=");
+            calculate();
+            text=displaystring->text();
+   }
+   if (str!="=") {
+    stack.push(text);
+    text+=str;
+    stack.push(str);
+    firststring->setText(text);
+   }
+  }
+ }
+ displaystring->setText(text);
+}
+
+
+void Calculator::clearAll() {
+    stack.clear(); displaystring->setText(""); firststring->setText("");
+}
+
+void Calculator::slotButtonClicked() {
+    QString str = ((QPushButton*)sender())->text();
+    if (str == "CE") clearAll();
+    else input(str);
+}
+
+void Calculator::keyPressEvent(QKeyEvent *event) {
+    int key=event->key();
+    QString str = QString(QChar(key));
+    if (key==Qt::Key_Delete) clearAll();
+    else input(str);
 }
 
--- calculator.h
+++ calculator.h
@@ -6,16 +6,24 @@
 #include <QPushButton>
 #include <QGridLayout>
 
+#include <QKeyEvent>
+#include <Qt>
+
 
 class Calculator : public QWidget
 {
     Q_OBJECT
     private:
+     QLabel *firststring; //thr output of the privious operand or opration
      QLabel *displaystring; // will display the results of calculations
      QStack <QString> stack; //Storage of two numbers and selected operations on them
+protected:
+     virtual void keyPressEvent(QKeyEvent *event); //keystroke processing?obrabotka klavish
     public:
      Calculator (QWidget* pwgt = 0);
      QPushButton* createButton (const QString str);
+     void clearAll ();
+     void input(QString); //pressure button or key
      void calculate ();
     public slots:
      void slotButtonClicked (); //pressing the buttons
